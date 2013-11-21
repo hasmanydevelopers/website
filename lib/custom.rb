8cae223048e0b42ed788b7239ef5641fd456a7e6
LuciaMariana <lucia.m.lo.monaco@gmail.com>
2013-11-21 18:24:12 -0430

    Crear función para ordenar los items de un determinado tipo y de acuerdo a una lista de títulos, y usar función para obtener los proyectos en el home

--- lib/custom.rb
+++ lib/custom.rb
@@ -5,3 +5,26 @@ def nav_link_to_unless_current(text, path)
     "<li><a href='#{path}'>#{text}</a></li>"
   end
 end
+
+def sort_items_by_title(kind, initialTitles)
+  sortedItems = []
+  restItems = []
+  itemsOfKind = @items.select{|i| i[:kind] == kind}
+  initialTitles.each do |t|
+    item = itemsOfKind.select{|i| i[:title] == t}
+    sortedItems = sortedItems + item
+  end
+  itemsOfKind.each do |ik|
+    has_listed_title = false
+    initialTitles.each do |t|
+        if ik[:title] == t
+            has_listed_title = true
+            break
+        end
+    end
+    if !has_listed_title
+        restItems << ik
+    end
+  end
+  return sortedItems + restItems
+end
